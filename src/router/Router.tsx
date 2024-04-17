4dd97f76a8fedc5688f13f91ab3e008845969c5c
adharsh <adharshd100@gmail.com>
2024-04-18 01:58:36 +0530

    Router rewamped

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import { AddCourse, ViewCourse } from '../views'
+import { Navbar } from '../components';
 
 const Router = () => {
     return (
         <Routes>
-            <Route path='/'element={<ViewCourse />}/>
-            <Route path='/add-course'element={<AddCourse />}/>1
-        </Routes >
+            <Route path='/' element={<Navbar />}>
+                <Route path='view-course' element={<ViewCourse />} />
+                <Route path='add-course' element={<AddCourse />} />
+            </Route>
+        </Routes>
     )
 }
 
-export default Router
+export default Router;
